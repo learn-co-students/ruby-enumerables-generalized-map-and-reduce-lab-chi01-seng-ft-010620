0504ba13075d2a57edf429e3cd2e7cce5a87110e
Matthew Gizzi <matthewgizzi@verizon.net>
2019-12-15 01:29:10 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(array)
+  index = 0
+  new_array = []
+  while array[index]
+    new_array.push(yield(array[index]))
+    index += 1
+  end
+  new_array
+end
+
+def reduce(array, sv=nil)
+  if sv
+    sum = sv
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  while i < array.length
+    sum = yield(sum, array[i])
+    i += 1
+  end
+  sum
+end
